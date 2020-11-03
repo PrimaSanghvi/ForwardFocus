0a490a80f6cbe431660fef4d6cbe894aac21e0d3
PrimaSanghvigit config --global user.name PrimaSanghvi <primasanghvi905@gmail.com>
2020-11-03 12:41:31 +0530

    last commit

--- forwardfocus/settings.py
+++ forwardfocus/settings.py
@@ -75,17 +75,31 @@ WSGI_APPLICATION = 'forwardfocus.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'forwardfocus',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
-        'PORT': '5432'
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'forwardfocus',
+            'USER': 'postgres',
+            'PASSWORD':  'forwardfocus123',
+            'HOST': 'database-3.cz2uauodc6ij.ap-south-1.rds.amazonaws.com',
+            'PORT': '5432'
+        }
+    }
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 #aws: Primasanghvi123
 #heruko: Primasanghvi@123
@@ -146,3 +160,4 @@ DEFAULT_FROM_EMAIL = 'ForwardFocus Team <noreply@forwardfocus.com>'
 django_heroku.settings(locals())
 #https://git.heroku.com/forwardfocus.git
 #https://git.heroku.com/forwardfocuscc.git
+#https://forwardfocuscc.herokuapp.com/
